2ab941e79640e0587e00268ecd427b39b5ff7adf
Addison van den Hoeven <AVANDENHOEVE14@ubishops.ca>
2018-10-09 09:26:41 -0400

    Moved I18nextprovider

--- src/components/BilingualLayout.js
+++ src/components/BilingualLayout.js
@@ -1,23 +1,26 @@
 import React from 'react';
-import {I18n} from 'react-i18next';
+import i18n from '../i18n/i18n';
+import {I18n, I18nextProvider} from 'react-i18next';
 import PropTypes from 'prop-types';
 
 class BilingualLayout extends React.Component {
 
   render() {
     return(
-      <I18n ns={["translation"]}>
-        {
-          (t, {i18n}) => {
-            const { children } = this.props;
+      <I18nextProvider i18n={ i18n }>
+        <I18n ns={["translation"]}>
+          {
+            (t, {i18n}) => {
+              const { children } = this.props;
 
-            const ChildrenWithTranslation = React.Children.map(children, (child) =>
-              React.cloneElement(child, {t : t, i18n: i18n})
-            );
-            return(ChildrenWithTranslation);
-        }
-        }
-      </I18n>
+              const ChildrenWithTranslation = React.Children.map(children, (child) =>
+                React.cloneElement(child, {t : t, i18n: i18n})
+              );
+              return(ChildrenWithTranslation);
+          }
+          }
+        </I18n>
+      </I18nextProvider>
     );
   }
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import i18n from './i18n/i18n';
-import { I18nextProvider } from 'react-i18next';
 
 ReactDOM.render(
-  <I18nextProvider i18n={ i18n }>
     <App />
-  </I18nextProvider>
     , 
   document.getElementById('root')
 );
